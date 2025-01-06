d09230d1426e791bf0d09ebfcf422c7b69e21136
Zivi_J <juan-felipe.ruiz@eawag.ch>
2025-01-06 13:49:11 +0100

    change to 1 node

--- submit_job.sh
+++ submit_job.sh
@@ -3,11 +3,12 @@
 #SBATCH --account="em09"
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-core=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --hint=nomultithread
+#SBATCH --time=1:00:00
 #SBATCH --output=slurm/slurm_%j.out
 #SBATCH --error=slurm/slurm_%j.err
 export OMP_NUM_THREADS=12 #$SLURM_CPUS_PER_TASK
