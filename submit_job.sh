5372a38e9ab1e2775d4d186cf353bcc9d95f5ee8
Zivi_J <juan-felipe.ruiz@eawag.ch>
2025-01-06 11:35:47 +0100

    Submit Job

---
+++ submit_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account="em09"
+#SBATCH --constraint='gpu'
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-core=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --partition=normal
+#SBATCH --constraint=gpu
+#SBATCH --hint=nomultithread
+#SBATCH --output=slurm/slurm_%j.out
+#SBATCH --error=slurm/slurm_%j.err
+export OMP_NUM_THREADS=12 #$SLURM_CPUS_PER_TASK
+cd ${SCRATCH}/lit_ecology_classifier
+module purge
+module load daint-gpu cray-python
+source lit_ecology/bin/activate
+python -m lit_ecology_classifier.main --max_epochs 2 --dataset ZooLakeTest --priority config/priority.json  --datapath $HOME/store/empa/em09/aquascope/zoo3.tar --batch_size 128 
