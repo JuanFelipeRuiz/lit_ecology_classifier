50926f97e4d416c98933cd3acc5999b63412c2d9
ZiviJ <juan-ruiz@gmx.ch>
2025-01-13 18:04:17 +0100

    Finished split.py to let it run with args

--- lit_ecology_classifier/split.py
+++ lit_ecology_classifier/split.py
@@ -5,7 +5,7 @@ the lit_ecology_classifier package. To use custom strategies, follow the templat
 provided in the filter or split_strategy base class and add the strategy to the
 strategies directory. 
 """
-
+import typing
 import logging
 import pathlib
 import sys
@@ -35,29 +35,21 @@ if __name__ == "__main__":
     # prepare file and folder paths
     image_overview_path = pathlib.Path(args.dataset_name)/args.overview_filename
 
-    split_folder_path = pathlib.Path(args.dataset_name)/"splits"
-    split_overview_path = pathlib.Path(args.dataset_name) / "splits"/f"{args.dataset_name}_split_overview.csv"
+    split_overview_path = pathlib.Path(args.dataset_name)/f"split_overview.csv"
 
     pathlib.Path(args.dataset_name).mkdir(parents=True, exist_ok=True)
-    pathlib.Path(split_folder_path).mkdir(parents=True, exist_ok=True)
-
-    if not split_overview_path.exists():
-    
-        new_entry = {
-            "split_strategy": None,
-            "filter_strategy": None,
-            "combined_split_hash": None,
-            "description": None,
-            "class_map": None
-        }
-
-        pd.DataFrame(new_entry, index=[0]).to_csv(split_overview_path)
-        
 
     split_processor = SplitProcessor(
-                                split_overview = split_overview_path,
-                                split_folder = split_folder_path,
-                                image_overview= image_overview_path,
-                                split_strategy= args.split_strategy,
-                                filter_strategy=  args.filter_strategy
+                                split_overview_path = split_overview_path,
+                                image_overview = image_overview_path,
+                                split_hash = args.split_hash,
+                                split_strategy = args.split_strategy,
+                                filter_strategy =  args.filter_strategy,
+                                split_args= args.split_args,
+                                filter_args= args.filter_args,
+                                class_map= args.class_map,
+                                priority_classes= args.priority_classes,
+                                rest_classes= args.rest_classes
                                 )
+    
+    split_processor.save_split(description= args.description)
