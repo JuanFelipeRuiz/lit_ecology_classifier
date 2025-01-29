30ce83891f754e34d4963800cf06a18cf84c10bb
ZiviJ <juan-ruiz@gmx.ch>
2025-01-29 17:29:54 +0100

    Change set up to try first to download a model backbone, to simplify the testing on clusters where the nodes have internet connections (euler e.g.) and the loading of trained statedict when given.

--- lit_ecology_classifier/models/setup_model.py
+++ lit_ecology_classifier/models/setup_model.py
@@ -1,6 +1,9 @@
 
 
 import logging
+from typing import Union
+from pathlib import Path
+
 
 import numpy as np
 import timm
@@ -9,6 +12,7 @@ from safetensors.torch import load_file
 
 
 logger = logging.getLogger(__name__)
+
 
 def setup_model(
     dropout_1,
@@ -19,126 +23,241 @@ def setup_model(
     pretrained=False,
     num_classes=None,
     checkpoint_path="checkpoints/backbone.safetensors",
+    architecture = "beitv2",
    
     **kwargs,
 ):
+    return SetupModel(
+        dropout_1,
+        dropout_2,
+        fc_node,
+        add_layer,
+        last_layer_finetune,
+        pretrained=pretrained,
+        num_classes=num_classes,
+        checkpoint_path=checkpoint_path,
+        architecture=architecture,
+        **kwargs,
+    ).setup_model()
+
+class SetupModel:
     """
-    Set up and return the specified model architecture.
+    Set up the model architecture and the trainable parameters for the model.
 
-    Args:
-        architecture (str): The model architecture to use.
-        main_param_path (str): Path to the directory containing main parameters.
-        ensemble (bool): Whether to use model ensembling.
-        finetune (bool): Whether to finetune the model or use it as is.
-        dataset (str): The name of the dataset.
-        testing (bool, optional): Set to True if in testing mode. Defaults to False.
-        train_first (bool, optional): Set to True to train the first layer of the model. Defaults to False.
-
-    Returns:
-        model: The configured model.
+    Arugments:
+        architecture: The base architecture / backbone of the model to be used.
+        add_layer: A flag to indicate if additional layers should be added to the model.
+        num_classes: The number of classes in the dataset.
+        checkpoint_path: The path to the local checkpoint file with the model weights.
+                        Not to be confused with the checkpoint file after training, since a regular checkpoint file of a model
+                        can just be loaded with pytorch_lightning.load_from_checkpoint() 
+        trained_weights_path: The path to the trained weights of the model.
+        last_layer_finetune: A flag to indicate if only the last layer should be finetuned.
+        dropout_1: The dropout rate for the first dropout layer.
+        dropout_2: The dropout rate for the second dropout layer.
+        fc_node: The number of nodes in the fully connected layer.
+        pretrained: Unused yet.
     """
-    # The slurm nodes cant download files directly currently so we make an extremly ugly hack
-    # first the ckpt is download with get_model.sh, then the model is initialised with random weights
-    model = timm.models.beit_base_patch16_224(pretrained=False, num_classes=1000)
 
-    # Load the checkpoint manually
-    checkpoint = load_file(checkpoint_path)
-    model.load_state_dict(checkpoint)
+    def __init__( self,
+            architecture = "beitv2",
+            checkpoint_path="checkpoints/backbone.safetensors",
+            num_classes=None,
+            add_layer = False,
+            last_layer_finetune = False,
+            dropout_1 = 0.4,
+            dropout_2 = 0.3,
+            fc_node = 512,
+            pretrained=False,
+            trained_weights_path = None,
+            **kwargs,
+        ):
+    
+        self.dropout_1 = dropout_1
+        self.dropout_2 = dropout_2
+        self.fc_node = fc_node
+        self.add_layer = add_layer
+        self.last_layer_finetune = last_layer_finetune
+        
+        self.num_classes = num_classes
+        self.checkpoint_path = checkpoint_path
+        self.architecture = architecture
+        self.trained_weights_path = trained_weights_path
+        self.model = None
 
-    # Remove the head
-    del checkpoint["head.weight"]
-    del checkpoint["head.bias"]
+        # not used yet
+        self.pretrained = pretrained
 
-    # Load the remaining state dict
-    model.load_state_dict(checkpoint, strict=False)
+    def setup_model(self):
+        """ Set up the model architecture and the trainable parameters for the model.
 
-    # Modify the model to match the number of classes in your dataset
-    model.head = torch.nn.Linear(model.head.in_features, num_classes)
+        Orchestrates the preparation of the model backbone, adding additional layers, and setting the trainable parameters.
+        based on the given arguments.
 
-    if add_layer:
-        # add additional layers
-        model = add_additional_layers(model, num_classes, **kwargs)
+        Returns:
+            torch.nn.Module: The model with the prepared architecture and trainable parameters.
+        """
+        self.prepare_model_backbone()
 
-    # Set the trainable parameters
-    set_trainable_params(
-        model,
-        add_layer = add_layer, 
-        last_layer_finetune= last_layer_finetune
+        if self.add_layer:
+            self.add_additional_layers()
+
+        self.set_trainable_params()
+
+        if self.trained_weights_path:
+            self.load_model_weights
+
+        return self.model
+    
+
+    def load_model_weights(self):
+        """
+        Load the state dict of the model into the prepared model.
+        """
+        state_dict = torch.load(self.trained_weights_path, map_location="cpu")
+        self.model.load_state_dict(state_dict)
+
+
+    def prepare_backbone_from_huggingface(self):
+        """
+        Prepare the model from a cached backbone or download from huggingface.
+        using the timm library.
+        """
+
+        # dictionary mapping model names to huggingface model names
+        model_mapping = {
+            'deit': 'deit_base_distilled_patch16_224.fb_in1k',
+            'efficientnetb2': 'tf_efficientnet_b2.ns_jft_in1k',
+            'efficientnetb5': 'tf_efficientnet_b5.ns_jft_in1k',
+            'efficientnetb6': 'tf_efficientnet_b6.ns_jft_in1k',
+            'efficientnetb7': 'tf_efficientnet_b7.ns_jft_in1k',
+            'densenet': 'densenet161.tv_in1k',
+            'mobilenet': 'mobilenetv3_large_100.miil_in21k_ft_in1k',
+            'inception': 'inception_v4.tf_in1k',
+            'vit': 'vit_base_patch16_224.augreg2_in21k_ft_in1k',
+            'mae': 'vit_base_patch16_224.mae',
+            'swin': 'swin_base_patch4_window7_224.ms_in22k_ft_in1k',
+            'beit': 'beit_base_patch16_224.in22k_ft_in22k_in1k',
+            'beitv2': 'beitv2_base_patch16_224.in1k_ft_in22k'
+        }
+
+        if self.architecture in model_mapping:
+            self.model = timm.create_model(model_mapping[self.architecture], pretrained=True, num_classes=self.num_classes)
+        else:
+            logger.warning("%s not implemented and tested, please check the compatibility with the package. \
+                           Trying to prepare the model backbone with timm.", self.architecture)
+            try:
+                self.model = timm.create_model(self.architecture, pretrained=True, num_classes=self.num_classes)
+            except:
+                raise NotImplementedError(f"Architecture {self.architecture} could not be prepared from huggingface.")
+            
+    def prepare_backbone_from_local_checkpoint(self):
+        """
+        Set up the model from a local checkpoint file for a beitV2 architecture.
+
+        
+        Currently (2024) the slurm nodes from CSCS cant download files directly, so we make an
+        extremly ugly hack. First the checkpoint, a dictionary like file with the model weights / state dict
+        need to be downloaded from huggingface. Can be done manually or with get_model.sh The model is then
+        initialised with random weights and the head respectively the last layerare adjusted to match the 
+        number of classes in the dataset.
+        """
+
+        if self.architecture == "beitv2":
+
+            # The slurm nodes cant download files directly currently so we make an extremly ugly hack
+            # first the ckpt is download with get_model.sh, then the model is initialised with random weights
+            model = timm.models.beit_base_patch16_224(pretrained=False, num_classes=1000)
+
+            # Load the checkpoint manually
+            checkpoint = load_file(self.checkpoint_path)
+            model.load_state_dict(checkpoint)
+
+            # Remove the head
+            del checkpoint["head.weight"]
+            del checkpoint["head.bias"]
+
+            # Load the remaining state dict
+            model.load_state_dict(checkpoint, strict=False)
+
+            # Modify the model to match the number of classes in your dataset
+            model.head = torch.nn.Linear(model.head.in_features, self.num_classes)
+
+
+            self.model = model
+
+        else:
+            raise NotImplementedError(f"Architecture {self. architecture} not implemented for local checkpoint setup.")
+            
+
+    def prepare_model_backbone(self):
+        """
+        Prepare the model backbone from using the timm library and huggingface. We assume that the preparation from huggingface
+        can fail because it is not cached or the internet connection is not available. In this case we try to prepare the model
+        from a local checkpoint.
+
+        This case happens using the CSCS slurm nodes, where the nodes have no direct internet connection. (Extremly ugly hack)
+        #TODO: Find a better solution for the CSCS nodes
+        """
+
+        try:
+            logger.info("Preparing the model using a cached backbone or downloading from huggingface.")
+            self.prepare_backbone_from_huggingface()
+
+        except NotImplementedError as e:
+            logger.warning(e)
+            
+        # try to prepare the model from a local checkpoint, since it could have failed because of the missing internet connection
+        except:
+            logger.info("Preparing the model using a local checkpoint.")
+            self.prepare_backbone_from_local_checkpoint()
+
+
+    def add_additional_layers(self):
+        """
+        Add additional layers to the model for fine-tuning..
+        """
+
+        if self.architecture == "deit":
+            in_features = self.model.get_classifier()[-1].in_features
+            pretrained_layers = list(self.model.children())[:-2]
+        else:
+            in_features = self.model.get_classifier().in_features
+            pretrained_layers = list(self.model.children())[:-1]
+
+        additional_layers = torch.nn.Sequential(
+            torch.nn.Dropout(p=self.dropout_1),
+            torch.nn.Linear(in_features=in_features, out_features=self.fc_node),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Dropout(p=self.dropout_2),
+            torch.nn.Linear(in_features=self.fc_node, out_features=self.num_classes),
         )
+        self.model = torch.nn.Sequential(*pretrained_layers, additional_layers)
 
-    # Total parameters and trainable parameters
-    total_params = sum(p.numel() for p in model.parameters())
+    def set_trainable_params(self):
+        """
+        Set the trainable parameters of the model.
+        """
+        n_layer = 0
 
-    logger.info("%s total parameters.", total_params)
-    print(f"{total_params:,} total parameters.")
-    total_trainable_params = sum(
-        p.numel() for p in model.parameters() if p.requires_grad
-    )
+        # define if only the last layer should be finetuned
+        if self.last_layer_finetune:
 
-    logger.info("%s training parameters.", total_trainable_params)
-    print(f"{total_trainable_params:,} training parameters.")
+            # define the number of layers to unfreeze. increase nubmer if additional layers are added
+            layer_to_unfreeze = 2 if self.add_layer == False else 5
 
-    return model
+            # freeze all layers 
+            for param in self.model.parameters():
+                n_layer += 1
+                param.requires_grad = False
 
+            # unfreeze the last n layers 
+            for i, param in enumerate(self.model.parameters()):
+                if i + 1 > n_layer - layer_to_unfreeze:
+                    param.requires_grad = True
 
-def add_additional_layers(
-    model, 
-    num_classes,
-    dropout_1,
-    dropout_2,
-    fc_node,
-    architecture
-):
-    """
-    Add additional layers to the model.
-
-    Args:
-        model (nn.Module): The model to configure.
-        num_classes (int): The number of classes in the dataset.
-    """ 
-    logger.debug("Adding additional layers to the model.")
-
-    if architecture == "deit":
-        in_features = model.get_classifier()[-1].in_features
-        pretrained_layers = list(model.children())[:-2]
-    else:
-        in_features = model.get_classifier().in_features
-        pretrained_layers = list(model.children())[:-1]
-
-    additional_layers = torch.nn.Sequential(
-        torch.nn.Dropout(p=dropout_1),
-        torch.nn.Linear(in_features=in_features, out_features=fc_node),
-        torch.nn.ReLU(inplace=True),
-        torch.nn.Dropout(p=dropout_2),
-        torch.nn.Linear(in_features=fc_node, out_features=num_classes),
-    )
-    return torch.nn.Sequential(*pretrained_layers, additional_layers)
-
-
-def set_trainable_params(model, add_layer, last_layer_finetune):
-    """
-    Set the trainable parameters of the model.
-
-    Args:
-        model (nn.Module): The model to configure.
-        train_first (bool, optional): If True, train the first layer of the model. Defaults to False.
-        finetune (bool, optional): If True, finetune the model. Defaults to True.
-    """
-
-    n_layer = 0
-
-    if last_layer_finetune:
-
-        layer_to_unfreeze = 2 if add_layer == False else 5
-
-        for param in model.parameters():
-            n_layer += 1
-            param.requires_grad = False
-
-        for i, param in enumerate(model.parameters()):
-            if i + 1 > n_layer - layer_to_unfreeze:
+        else:
+            # unfreeze all layers
+            for param in self.model.parameters():
                 param.requires_grad = True
-
-    else:
-        for param in model.parameters():
-            param.requires_grad = True
+        
