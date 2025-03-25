5439ed15e3c507993a4d5d2ac75a3c4113ec3c6f
Juan <juan-ruiz@gmx.ch>
2025-03-25 13:53:06 +0100

    enhance loss definition and metrics to calculate for train and test

--- lit_ecology_classifier/models/model.py
+++ lit_ecology_classifier/models/model.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torchmetrics 
 import pandas as pd
 from datetime import datetime
 from lightning import LightningModule
@@ -32,58 +33,66 @@ class LitClassifier(LightningModule):
             
         else:
             self.class_map = self.hparams.class_map
+
         self.hparams.num_classes = len(self.class_map.keys())
         self.inverted_class_map = dict(sorted({v: k for k, v in self.class_map.items()}.items()))
         self.model = setup_model(**self.hparams)
         self.loss = self.define_loss()
+        self.train_metrics, self.val_metrics = self.define_metrics()
+
         time_stamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         self.hparams.model_name = f"{self.hparams.architecture}_{time_stamp}" if hasattr(self.hparams, "architecture") else f"model_{time_stamp}"
         
         logging.info("Model initialized with hyperparameters:\n {}".format(pprint.pformat(self.hparams)))
-        self.cls_report = None
+
+       
+    def define_metrics(self):
+        """
+        Defines the calculated metrics for the model for training and validation.
+        """
+
+        # Define the task type based on the number of classes
+        task_type = "multiclass" if self.hparams.num_classes > 2 else "binary"
+
+        train_metrics = torchmetrics.MetricCollection({
+            "acc": torchmetrics.Accuracy(num_classes=self.hparams.num_classes, task= task_type),
+            "f1": torchmetrics.F1Score(num_classes=self.hparams.num_classes, average="weighted", task= task_type),
+            "balanced_acc": torchmetrics.Accuracy(num_classes=self.hparams.num_classes, average="macro", task= task_type)
+        }, prefix="train_")
+
+        val_metrics = torchmetrics.MetricCollection({
+            "acc": torchmetrics.Accuracy(num_classes=self.hparams.num_classes, task= task_type),
+            "f1": torchmetrics.F1Score(num_classes=self.hparams.num_classes, average="weighted", task= task_type),
+            "balanced_acc": torchmetrics.Accuracy(num_classes=self.hparams.num_classes, average="macro", task= task_type),
+            "precision": torchmetrics.Precision(num_classes=self.hparams.num_classes, average="macro", task= task_type)
+        }, prefix="val_")
+
+        return train_metrics, val_metrics
+
 
     def define_loss(self):
         """
-        Define the loss function for the model.
-        Args:
-            class_weights (torch.Tensor): Class weights for the loss function.
+        Define the loss function to use for training.
+        
+        If the loss is set to "focal", use the FocalLoss class with gamma=1.75.
+        If class weights are provided, use the CrossEntropyLoss with the class weights.
+        Otherwise, use the standard CrossEntropyLoss.
         """
-        if hasattr(self, "hparams") and getattr(self.hparams, "loss", None) == "focal":
-            if hasattr(self.hparams, "class_weights"):
-                print("Focal loss cannot be used with class weights.")
+        loss_type = getattr(self.hparams, "loss", None)
+        class_weights = getattr(self.hparams, "class_weights", None)
+
+        if loss_type == "focal":
+            if class_weights is not None:
                 raise ValueError("Focal loss cannot be used with class weights.")
+            logging.info("Using FocalLoss")
             return FocalLoss(alpha=None, gamma=1.75)
-        else:
-            if hasattr(self.hparams, "class_weights") and self.hparams.class_weights is not None:
-                print("Using class weights")
-                return torch.nn.CrossEntropyLoss(weight=self.hparams.class_weights)
-            print("Using standard cross entropy loss")
-            return torch.nn.CrossEntropyLoss()
 
-    def TTA(self, batch):
-        """
-        Perform Test Time Augmentation (TTA) on the input batch.
-        Args:
-            batch (tuple): Input batch containing images and labels.
-        Returns:
-            torch.Tensor: Geometrics Average of probabilities from the TTA predictions.
-            torch.Tensor: True labels if batch is list containg true labels as second entry else None.
-        """
-        x = torch.cat([batch[str(i * 90)] for i in range(4)], dim=0)
-        logits = self(x).softmax(dim=1)
-        logits = torch.stack(torch.chunk(logits, 4, dim=0))
-        logits = gmean(logits, dim=0)
-        return logits
+        if class_weights is not None:
+            logging.info("Using CrossEntropyLoss with class weights.")
+            return torch.nn.CrossEntropyLoss(weight=class_weights)
 
-    def forward(self, x):
-        """
-        Forward pass through the model.
-        Args:
-            x (torch.Tensor): Input tensor.
-        Returns:
-            torch.Tensor: Model output.
-        """
-        return self.model(x)
+        logging.info("Using CrossEntropyLoss")
+        return torch.nn.CrossEntropyLoss()
 
     def configure_optimizers(self):
         """
@@ -94,49 +103,91 @@ class LitClassifier(LightningModule):
         """
         optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.hparams.lr)
 
-        #scheduler = CosineWarmupScheduler(optimizer, warmup=3 * len(self.datamodule.train_dataloader()), max_iters=self.trainer.max_epochs * len(self.datamodule.train_dataloader()))
-        #lr_scheduler_config = {
-        #    "scheduler": scheduler,
-        #    "interval": "step",
-        #    "frequency": 1,
-        #}
-        return [optimizer]#, [lr_scheduler_config]
+        scheduler = CosineWarmupScheduler(optimizer, warmup=3 * len(self.datamodule.train_dataloader()), max_iters=self.trainer.max_epochs * len(self.datamodule.train_dataloader()))
+        lr_scheduler_config = {
+            "scheduler": scheduler,
+            "interval": "step",
+            "frequency": 1,
+        }
+        return [optimizer], [lr_scheduler_config]
+
+    def TTA(self, batch):
+        """
+        Perform Test Time Augmentation (TTA) on the input batch.
+
+        Args:
+            batch (tuple): Input batch containing the rotation as key and the probability as value.
+
+        Returns:
+            torch.Tensor: Geometrics Average of probabilities from the TTA predictions.
+            torch.Tensor: True labels if batch is list containg true labels as second entry else None.
+        """
+
+        # extract the probabilities from each rotation
+        x = torch.cat([batch[str(i * 90)] for i in range(4)], dim=0)
+
+        # use a softmax function to convert the logits to probabilities
+        logits = self(x).softmax(dim=1)
+
+        logits = torch.stack(torch.chunk(logits, 4, dim=0))
+
+        # calculate the geometric mean of the probabilities
+        logits = gmean(logits, dim=0)
+        return logits
+
+    def forward(self, x):
+        """
+        Forward pass through the model.
+        """
+        return self.model(x)
 
     def load_datamodule(self, datamodule):
         """
-        Load the data module into the model.
-        Args:
-            datamodule (LightningDataModule): Data module to load.
+        Load the data module into the model to save the necessary hyperparameters
+        for a new run.
         """
         self.datamodule = datamodule
-        self.hparams.TTA = self.datamodule.TTA
-        self.hparams.datapath = self.datamodule.datapath
-        self.hparams.splits = self.datamodule.splits
-        self.hparams.batch_size = self.datamodule.batch_size
+        self.hparams.TTA = datamodule.TTA
+        self.hparams.datapath = datamodule.datapath
+        self.hparams.splits = datamodule.splits
+        self.hparams.batch_size = datamodule.batch_size
 
     def training_step(self, batch, batch_idx):
         """
-        Perform a training step.
+        Perform a training step and log the calculated loss and metrics.
+
         Args:
             batch (tuple): Input batch containing images and labels.
             batch_idx (int): Batch index.
+
         Returns:
             torch.Tensor: Computed loss for the batch.
         """
         x, y = batch
         logits = self(x)
+        
+        # compute the loss 
         loss = self.loss(logits, y)
+
+        # compute the train metrics with the predfined metrics inside of define_metrics
+        train_metrics = self.train_metrics(logits, y)
+
+        # log the loss and metrics
         self.log("train_loss", loss, on_step=True, on_epoch=False, prog_bar=True, logger=True, sync_dist=True)
-        acc = (logits.argmax(dim=1) == y).float().mean()
-        self.log("train_acc", acc, on_step=True, on_epoch=False, prog_bar=True, logger=True, sync_dist=True)
+        self.log_dict(train_metrics, on_step=True, on_epoch=False, prog_bar=True, logger=True, sync_dist=True)
         return loss
+
+    def on_train_epoch_end(self):
+        
+        # reset the metrics at the end of the epoch
+        self.train_metrics.reset()
+        return super().on_train_epoch_end()
+        
 
     def on_validation_epoch_start(self):
         self.val_step_predictions = []
         self.val_step_targets = []
         self.val_step_probs = []
-
-            
 
     def validation_step(self, batch, batch_idx):
         """
@@ -156,18 +207,21 @@ class LitClassifier(LightningModule):
             x, y = batch
             logits = self(x)
             probs=logits.softmax(dim=1)
+
         loss = self.loss(logits, y)
+
+
+        self.val_metrics.update(probs, y)
+        step_metrics = self.val_metrics.compute()
+
         self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
-        acc = (probs.argmax(dim=1) == y).float().mean()
-        self.log("val_acc", acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
-        f1 = f1_score(y.cpu(), probs.argmax(dim=1).cpu(), average="weighted")
-        self.log("val_f1", f1, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+        self.log_dict(step_metrics, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
 
-        self.val_step_probs.append(probs.cpu())
-        self.val_step_predictions.append(probs.cpu().argmax(dim=1))
-        self.val_step_targets.append(y.cpu())
+        self.val_step_probs.append(probs)
+        self.val_step_predictions.append(probs.argmax(dim=1))
+        self.val_step_targets.append(y)
 
-        return {"val_loss": loss, "val_acc": acc, "val_f1": f1, "probs": probs, "y": y}
+        return loss
 
     def on_validation_epoch_end(self):
         """
@@ -181,9 +235,16 @@ class LitClassifier(LightningModule):
 
         fig_score = plot_score_distributions(all_scores, all_preds, self.inverted_class_map.values(), all_labels)
 
-        balanced_acc = balanced_accuracy_score(all_labels.cpu().numpy(), all_preds.cpu().numpy())
+        metrics = self.val_metrics.compute()
+        self.log_dict(metrics, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
 
+        
+        
+        balanced_acc = balanced_accuracy_score(all_labels.cpu().numpy(), all_preds.cpu().numpy())
+        
         self.log("val_balanced_acc", balanced_acc, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
+
+
 
         precision = torch.sum((all_preds!= 0) & (all_labels!=0) ).item()/max(torch.sum((all_preds!= 0) & (all_labels!=0) ).item()+torch.sum((all_preds != 0) & (all_labels == 0)).item(),1)
         self.log("val_precision", precision, on_step=False, on_epoch=True, prog_bar=True, logger=True, sync_dist=True)
@@ -192,7 +253,6 @@ class LitClassifier(LightningModule):
         del confusion_matrix, confusion_matrix_norm
         # Log the confusion matrix to wandb if use_wandb is true
         if self.hparams.use_wandb:
-
             self.logger.log_image(key=f"score_distributions", images=[fig_score], step=self.current_epoch)
             self.logger.log_image(key="confusion_matrix", images=[fig1], step=self.current_epoch)
             self.logger.log_image(key="confusion_matrix_norm", images=[fig2], step=self.current_epoch)
@@ -209,7 +269,7 @@ class LitClassifier(LightningModule):
                 fig_score.savefig(score_distributions_epoch_path)
             
             elif self.current_epoch == self.trainer.max_epochs - 1:
-                    self.train_outpath = Path(self.hparams.train_outpath) / self.model_name
+                    self.train_outpath = Path(self.hparams.train_outpath) / self.hparams.model_name
                     Path.mkdir(self.train_outpath, exist_ok=True)
                     confusion_matrix_epoch_path = self.train_outpath / f"confusion_matrix_epoch_{self.current_epoch}.png"
                     confusion_matrix_norm_epoch_path = self.train_outpath  / f"confusion_matrix_normalized_epoch_{self.current_epoch}.png"
@@ -226,7 +286,19 @@ class LitClassifier(LightningModule):
 
         plt.close(fig1)
         plt.close(fig2)
-        plt.close(fig_score)            
+        plt.close(fig_score)
+
+    def on_fit_end(self) -> None:
+        """
+        If the model is not using wandb, plot the loss and accuracy curves at the end of training
+        and save them in the output folder.
+        """
+        if not self.hparams.use_wandb:
+            plot_loss_acc(self.trainer.logger)
+            plt.show(plot_loss_acc)
+            print(f"Artefatcs saved to {self.trainer.log_dir}")
+            
+        return super().on_fit_end()    
 
     def on_test_epoch_start(self):
         """
@@ -284,7 +356,6 @@ class LitClassifier(LightningModule):
         )
 
 
-        # Compute metrics
         balanced_acc, false_positives, precision, recall, f1, accuracy, all_y_labels_np, all_predicted_labels_np,cls_report  = compute_metrics(all_y_labels, all_pred_label, self.inverted_class_map)
 
         
@@ -313,8 +384,6 @@ class LitClassifier(LightningModule):
 
         roc_auc_binary = compute_roc_auc_binary(all_y_labels, all_scores)
 
-
-
         inverted_predicted_labels_np = np.array([self.inverted_class_map[label] for label in all_predicted_labels_np])
         inverted_y_labels_np = np.array([self.inverted_class_map[label] for label in all_y_labels_np])
 
@@ -322,20 +391,10 @@ class LitClassifier(LightningModule):
         classifications = zip(filenames, inverted_y_labels_np, inverted_predicted_labels_np, all_highest_scores.numpy() , all_scores.numpy())
         
         #classifications = test_output_results(im_names=filenames, true_labels=all_y_labels, predicted_labels=inverted_labels_np, scores=all_scores)
-        
-       
-    
+
         self.test_f1 = f1
         self.test_acc = accuracy
         self.test_cell = Path(self.hparams.datapath).name
-        print("test_roc_auc_binary:", roc_auc_binary)
-        print("test_balanced_acc:", balanced_acc)
-        print("test_false_positives:", false_positives)
-        print("test_acc:", accuracy)
-        print("test_f1:", f1)
-        print("test_auc_macro:", roc_auc)
-        print("test_precision:", precision)
-        print("test_recall:", recall)
 
         if self.hparams.use_wandb:
             self.logger.log_image(key=f"score_distributions", images=[fig_score], step=self.current_epoch)
@@ -376,12 +435,10 @@ class LitClassifier(LightningModule):
         return super().on_test_epoch_end()
             
 
-
     def on_predict_start(self) -> None:
         """
-        Hook for the start of the inference phase.
+        Hook for the start of the prediction process.
         """
-
         self.probabilities = []
         self.model.eval()
 
@@ -394,7 +451,8 @@ class LitClassifier(LightningModule):
             batch (tuple): Input batch containing images
         """
         with torch.no_grad():
-
+            
+            # if TTA is enabled, call the TTA method to calculate the mean of the predictions
             if self.hparams.TTA:
                 probs = self.TTA(batch).cpu()
 
@@ -406,8 +464,7 @@ class LitClassifier(LightningModule):
 
     def on_predict_epoch_end(self) -> None:
         """
-        Hook to be called at the end of the test epoch.
-        Saves predicted labels in text file in folder Output
+        Hook for the end of the prediction process.
         """
         filenames = self.datamodule.predict_dataset.image_infos
         max_index = torch.cat(self.probabilities).argmax(axis=1)
@@ -427,13 +484,3 @@ class LitClassifier(LightningModule):
         barplot_predictions(pred_label, self.inverted_class_map, self.hparams.outpath)
         return super().on_predict_epoch_end()
 
-    def on_fit_end(self) -> None:
-        """
-        If the model is not using wandb, plot the loss and accuracy curves at the end of training
-        and save them in the output folder.
-        """
-        if not self.hparams.use_wandb:
-            plot_loss_acc(self.trainer.logger)
-            plt.show(plot_loss_acc)
-            print(f"Artefatcs saved to {self.trainer.log_dir}")
-        return super().on_fit_end()
