054a7d5d91c74e353665d1d1296bcb94f74a73c6
Juan <juan-ruiz@gmx.ch>
2025-05-22 14:26:02 +0200

    Class mapping,ensure that the class map starts from 0 and is coninuos

--- lit_ecology_classifier/helpers/helpers.py
+++ lit_ecology_classifier/helpers/helpers.py
@@ -395,10 +395,12 @@ def filter_class_mapping(
         if (key in priority_classes) or (key in rest_classes) or (rest_classes == [])  
     }
 
-    if "rest" not in class_map:
-        # extract 1 from the class values to create a class with the value 0
-        class_map = {key: value - 1 for key, value in class_map.items()}
-        
+    # sort the class map by the values
+    class_map = dict(sorted(class_map.items(), key=lambda item: item[1]))
+    
+    # transform the values to be continuous
+    class_map = {key: i for i, key in enumerate(class_map.keys())}
+
     return class_map
 
 def _extract_class_map(tar_or_dir_path):
--- tests/test_helper/test_helpers.py
+++ tests/test_helper/test_helpers.py
@@ -10,24 +10,24 @@ import lit_ecology_classifier.helpers.helpers as helpers
     ("rest_classes", "priority_classes", "expected_output"),
     [
         # return original class map if no rest classes are given
-        ([], [] , {"class_1": 1, "class_2": 2, "class_3": 3}),
+        ([], [] , {"class_a": 0, "class_b": 1, "class_c": 2}),
 
         # set class 1 as priority class and keep the rest classes
-        ([], ["class_1"], {"class_1": 1, "rest": 0}),
+        ([], ["class_a"], {"class_a": 1, "rest": 0}),
         
         # keep only class 1 and 2 with class values since they are priority classes
         # and remove class 3
-        (["class_1", "class_2"], [] , {"class_1": 1, "class_2": 2}),
+        (["class_a", "class_b"], [] , {"class_a": 0, "class_b": 1}),
 
         # keep class class 3 with original value and keep class 2 
-        (["class_2"], ["class_3"] , {"rest": 0, "class_3": 3})
+        (["class_b"], ["class_c"] , {"rest": 0, "class_c": 1})
     ]
 )
 
 
 def test_rest_class_filter(expected_output, priority_classes, rest_classes):
     
-    input_class_map = {"class_1": 1, "class_2": 2, "class_3": 3}
+    input_class_map = {"class_a": 1, "class_b": 2, "class_c": 3}
     
     assert helpers.filter_class_mapping( 
                             class_map= input_class_map,
